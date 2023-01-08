39fc00fe1a35bc6b7df14ac72ee44379de7957f6
DinkyDieDingus <57827814+DinkyDieDingus@users.noreply.github.com>
2023-01-08 15:05:21 +0800

    Initial commit

---
+++ src/components/Link.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: ['name', 'links']
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <ul>
+            <li v-for="link in links">
+                <a v-bind:href="link.url" target="_blank" rel="noopener noreferrer">{{ link.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    a {
+        color: inherit;
+    }
+
+    ul {
+        list-style-type: square;
+        margin-left: -20pt;
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Logo from '../components/Logo.vue'
+import Link from '../components/Link.vue'
+import axios from "axios"
+</script>
+<script>
+export default {
+  data() {
+    return {
+      linkData: {}
+    }
+  },
+  mounted() {
+    axios.get('/links.json').then((response) => {this.linkData=response.data})
+  }
+}
+</script>
+
+<template>
+  <main>
+    <Logo />
+    <div class="links">
+      <Link v-for="category in linkData.categories" :name="category.title" :links="category.links"/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .links {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    /*
+    gap: 40pt;
+    */
+  }
+</style>
